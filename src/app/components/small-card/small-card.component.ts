897dea493b534507ce8ca97695ea54201a0aa128
Shenrique1970 <shvribeiro@hotmail.com>
2023-05-18 14:19:56 -0300

    Aprendendo sobre rotas

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  id:string = "0"
+
+  construtor() {}
+
+  ngOnInit(): void {
+    
+  }
+}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title":"Angular 15",
+        "description":"Estou usando Angular 15",
+        "photoCover": "http://localhost:4200/assets/angular.jpeg"
+    },
+    {
+        "id": "2",
+        "title":"Serie antiga O Tunel do Tempo",
+        "description":"ola, curta series antigas",
+        "photoCover": "http://localhost:4200/assets/series.jpeg"
+    },
+    {
+        "id": "3",
+        "title":"Serie Arquivo X a melhor serie",
+        "description":"ola, curta series antigas",
+        "photoCover": "http://localhost:4200/assets/arquivox.jpeg"
+    },
+    {
+        "id": "4",
+        "title":"Serie antiga inesquecivel Ultraman Jack",
+        "description":"ola, curta series antigas",
+        "photoCover": "http://localhost:4200/assets/ultraman.jpg"
+    }
+]
